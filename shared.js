1c1ca7986c93ae989877bde1953e6e4db37615de
Istvan Acs <istvan.acs89@gmail.com>
2018-10-07 15:22:52 +0100

    Add eventlistener to the plan buttons

--- shared.js
+++ shared.js
@@ -1,3 +1,11 @@
 let backdrop = document.querySelector('.backdrop')
+let modal = document.querySelector('.modal')
+let selectPlanButton = document.querySelectorAll('.plan button')
+console.log(selectPlanButton)
 
-console.log(backdrop)
+for (let i = 0; i < selectPlanButton.length; i++) {
+    selectPlanButton[i].addEventListener('click', () => {
+        modal.style.display = 'block';
+        backdrop.style.display = 'block';
+    })
+}
