48300997073f5a35e7554ffd8b799febff72cac4
Istvan Acs <istvan.acs89@gmail.com>
2018-10-07 16:43:40 +0100

    Add click event to the hamburger menu

--- shared.js
+++ shared.js
@@ -2,16 +2,20 @@ let backdrop = document.querySelector('.backdrop')
 let modal = document.querySelector('.modal')
 let modalNoButton = document.querySelector('.modal__action--negative')
 let selectPlanButton = document.querySelectorAll('.plan button')
-console.log(selectPlanButton)
+let toggleButton = document.querySelector('.toggle-button')
+let mobileNav = document.querySelector('.mobile-nav')
 
 for (let i = 0; i < selectPlanButton.length; i++) {
     selectPlanButton[i].addEventListener('click', () => {
-        modal.style.display = 'block';
-        backdrop.style.display = 'block';
+        modal.style.display = 'block'
+        backdrop.style.display = 'block'
     })
 }
 
-backdrop.addEventListener('click', closeModal)
+backdrop.addEventListener('click', () => {
+    mobileNav.style.display = 'none'
+    closeModal()
+})
 
 modalNoButton.addEventListener('click', closeModal)
 
@@ -19,3 +23,8 @@ function closeModal() {
     backdrop.style.display = 'none'
     modal.style.display = 'none'
 }
+
+toggleButton.addEventListener('click', () => {
+    mobileNav.style.display = 'block'
+    backdrop.style.display = 'block'
+})
